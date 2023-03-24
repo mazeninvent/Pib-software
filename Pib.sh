8a5929ad465d63f1d79e59016f8a6d4c6411ab2f
mazeninvent <86280313+mazeninvent@users.noreply.github.com>
2023-03-24 11:58:56 +0200

    Update Pib.sh

--- Pib.sh
+++ Pib.sh
@@ -3,24 +3,21 @@
 #wget "https://raw.githubusercontent.com/mazeninvent/Pib-software/main/Pib.sh" install shell script
 #chmod +x ./Pib.sh
 #./Pib.sh
-
-
 #This shell script is used to install all Pib required software
-
 #Basic dependencies needed in installation 
-sudo apt update
-sudo apt upgrade
-sudo apt install python3-pip
-sudo apt install curl
-sudo apt install git
-sudo apt update
-sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-pip
+sudo apt-get install curl
+sudo apt-get install git
+sudo apt-get update
+sudo apt-get upgrade
 
 #ROS 2
 #step1
 locale  # check for UTF-8
 
-sudo apt update && sudo apt install locales
+sudo apt-get update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -31,21 +28,21 @@ sudo apt install software-properties-common
 sudo add-apt-repository universe
 
 #step3
-sudo apt update && sudo apt install curl
+sudo apt-get update && sudo apt-get install curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 #step4
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 #step5
-sudo apt update
-sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
 #step6
-sudo apt install ros-humble-desktop
+sudo apt-get install ros-humble-desktop
 
 #step7
-sudo apt install ros-dev-tools
+sudo apt-get install ros-dev-tools
 
 #step8
 source /opt/ros/humble/setup.bash
@@ -64,8 +61,8 @@ sudo dpkg -i brickv_linux_latest.deb
 #Tinkerforge python APIs
 wget https://download.tinkerforge.com/apt/$(. /etc/os-release; echo $ID)/tinkerforge.gpg -q -O - | sudo tee /etc/apt/trusted.gpg.d/tinkerforge.gpg > /dev/null
 echo "deb https://download.tinkerforge.com/apt/$(. /etc/os-release; echo $ID $VERSION_CODENAME) main" | sudo tee /etc/apt/sources.list.d/tinkerforge.list
-sudo apt update
-sudo apt install python3-tinkerforge
+sudo apt-get update
+sudo apt-get install python3-tinkerforge
 
 #Depth-AI
 sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
